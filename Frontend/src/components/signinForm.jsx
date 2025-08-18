e2150ba4820963442b79f9b9524e710d2c4697e8
Shaprek <prekjainsha190994@gmail.com>
2025-08-19 04:46:55 +0530

    changed dashboard setting

--- Frontend/src/components/signinForm.jsx
+++ Frontend/src/components/signinForm.jsx
@@ -40,7 +40,7 @@ const SignInForm = ({ onClose }) => {
 });
 
         if (res.data.success) {
-  window.location.href = "http://localhost:5174"; // Or the deployed dashboard link
+  window.location.href = "https://grow-paisa.vercel.app/"; // Or the deployed dashboard link
 
 
         } else {
